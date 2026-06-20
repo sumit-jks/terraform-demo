8623ac06d870d5c7938616f8bb8dd286b4b87c97
sumit-jks <sumit.jks2k5@gmail.com>
2026-06-21 00:17:22 +0530

    re-order files

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_resource_group" "example2" {
+  name     = "example-resources"
+  location = "West Europe"
+}
