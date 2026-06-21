a2f62d9d9d83d3bbbe1dcb2393c56ef4039a403c
SUMIT KR SINGH <90760023+sumit-jks@users.noreply.github.com>
2026-06-21 12:45:59 +0530

    Comment out resource group definition in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "example" {
   location = "West Europe"
 }
 
-resource "azurerm_resource_group" "example2" {
-  name     = "example-resources"
-  location = "West Europe"
-}
+#resource "azurerm_resource_group" "example2" {
+#  name     = "example-resources"
+ # location = "West Europe"
+#}
