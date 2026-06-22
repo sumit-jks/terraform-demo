df2313a0228154700c0f20c58d1fe062ed4b823d
SUMIT KUMAR Singh (TOL1 R) <sumit.k.singh@bt.com>
2026-06-22 17:43:23 +0530

    modified main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,8 @@ resource "azurerm_resource_group" "example47" {
   location = "West Europe"
 }
 
-#resource "azurerm_resource_group" "example2" {
-#  name     = "example-resources"
- # location = "West Europe"
-#}
+resource "azurerm_resource_group" "example2" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+git 
