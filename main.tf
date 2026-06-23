52b1196d9a8eb6ff2dc2be962a7739486f0f3284
sumit-jks <sumit.jks2k5@gmail.com>
2026-06-23 23:45:24 +0530

    added a vnet again

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "azurerm_resource_group" "tutorial" {
   location = "West Europe"
 }
 
-resources "azurerm_virtual_network" "tutorial" {
+resource "azurerm_virtual_network" "tutorial" {
   name                = "tutorial-vnet"
   address_space       = ["10.0.0.0/16"]
 }
