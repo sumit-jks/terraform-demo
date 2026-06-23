f39c1cc39e6f33068de00d1e1d9df8d05643b317
sumit-jks <sumit.jks2k5@gmail.com>
2026-06-24 00:15:54 +0530

    backend

--- provider.tf
+++ provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = "4.67.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "tf-state-rg"
+    storage_account_name = "tfstatestorage2406"
+    container_name       = "tfstate"
+    key                  = "project1.tfstate"
+  }
 }
 
 provider "azurerm" {
