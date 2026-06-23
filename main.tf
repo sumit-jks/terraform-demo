f9b772405e4c58746e3072b2d78d8b4e33f7f9f1
sumit-jks <sumit.jks2k5@gmail.com>
2026-06-23 23:51:29 +0530

    added a vnet again4

--- main.tf
+++ main.tf
@@ -9,3 +9,17 @@ resource "azurerm_virtual_network" "tutorial" {
   resource_group_name = azurerm_resource_group.tutorial.name
   address_space       = ["10.0.0.0/16"]
 }
+
+resource "azurerm_subnet" "frontend" {
+  name                 = "frontend-subnet"
+  resource_group_name  = azurerm_resource_group.tutorial.name
+  virtual_network_name = azurerm_virtual_network.tutorial.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+resource "azurerm_subnet" "azureBastionSubnet" {
+  name                 = "AzureBastionSubnet"
+  resource_group_name  = azurerm_resource_group.tutorial.name
+  virtual_network_name = azurerm_virtual_network.tutorial.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
