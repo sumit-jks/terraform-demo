b0f23232a921f1f62160a9e6f3115d1d26614485
sumit-jks <sumit.jks2k5@gmail.com>
2026-07-08 20:12:00 +0530

    removed backend block

--- provider.tf
+++ provider.tf
@@ -5,13 +5,6 @@ terraform {
       version = "4.67.0"
     }
   }
-
-  backend "azurerm" {
-    resource_group_name  = "tf-state-rg"
-    storage_account_name = "tfstatestorage2406"
-    container_name       = "tfstate"
-    key                  = "project1.tfstate"
-  }
 }
 
 provider "azurerm" {
