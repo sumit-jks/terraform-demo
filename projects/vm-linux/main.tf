a053956638992de930fafff738df77a99bd14d9f
sumit-jks <sumit.jks2k5@gmail.com>
2026-07-08 20:35:50 +0530

    test pipeline

---
+++ projects/vm-linux/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_version = ">= 1.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.location
+}
---
+++ projects/vm-linux/terraform.tfvars
@@ -0,0 +1,2 @@
+rg_name  = "rg-sumit-vm-linux"
+location = "East US"
